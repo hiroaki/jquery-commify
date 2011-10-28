a835f50427ad09b94d3ae5e1cfb1a532c9d99336
WATANABE Hiroaki <hwat@mac.com>
2011-10-29 01:57:30 +0900

    target to convert only by selections
    日本語でOK?

--- jquery.commify.js
+++ jquery.commify.js
@@ -7,36 +7,26 @@
  *  http://www.opensource.org/licenses/mit-license.php
  *
  * Revision: $Rev$
- * Version: 0.0.1
+ * Version: 0.0.2
  *
 */
 (function($){
     $.fn.commify = function(){
-        var _commify = (function (){
-            return function (matched,cap){
-                    if( matched.match(/\..*\./) ){
-                        return matched;
-                    } 
-                    while(cap != (cap = cap.replace(/^(-?\d+)(\d{3})/, '$1,$2')));
-                    return cap;
+        var _commify = function (matched,cap){
+                if( matched.match(/\..*\./) || matched.match(/,/) ){
+                    return matched;
                 }
-            })();
-        var _digdag = (function (){
-            return function (elem){
-                    if( elem.nodeType == 3 ){ // if text node
-                        $(elem).replaceWith( $(elem).text().replace(/([0-9\.]+)/g, _commify ) );
-                    }else{
-                        $(elem).contents().each(function (){
-                            _digdag(this);
-                        })
-                    }
-                }
-            })();
+                while(cap != (cap = cap.replace(/^(-?\d+)(\d{3})/, '$1,$2')));
+                return cap;
+            };
 
         $(this).each(function(){
-            _digdag(this);
+            $(this).contents().each(function (){
+                if( this.nodeType == 3 ){ // if text node
+                    $(this).replaceWith( $(this).text().replace(/([0-9\.,]+)/g, _commify ) );
+                }
+            });
         });
-        
         return this;
     };
 })(jQuery);
